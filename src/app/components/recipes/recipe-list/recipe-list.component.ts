cc96311adc515e102d87f03d5554db0c1e5d60a4
Eyobell <eyobell.berhe@live.longwood.edu>
2018-10-05 19:01:23 -0400

    page/s template

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A test', 'This is a test', 'https://www.flightclub.com/media/catalog/product/2/0/201519_1.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../../shared/ingredients.model'
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Celery', 1.59)
+  ]
   constructor() { }
 
   ngOnInit() {
